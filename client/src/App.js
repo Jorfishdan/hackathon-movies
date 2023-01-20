86433e33c43f5f7890d27b2465cbbe300d28e45d
Jorfishdan <jordan_fish@ymail.com>
2023-01-20 07:56:16 -0800

    updated hompage to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-// import HomePage from "./pages/Homepage/Homepage"
-import About from "./pages/Homepage/Homepage"
-import NotFound from './pages/NotFound';
-import NavBar from './components/NavBar/NavBar';
-import HomePage from './components/HomePage/HomePage';
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import About from "./pages/Homepage/Homepage";
+import NotFound from "./pages/NotFound";
+import NavBar from "./components/NavBar/NavBar";
+// import HomePage from './components/HomePage/HomePage';
+import HomePage from "./pages/Homepage/Homepage";
 function App() {
   return (
     <>
-
-    <NavBar />
-    <HomePage />
-    {/* <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomePage />}/>
-    <Route path="/movie/:id" element={<HomePage />}/>
-    <Route path="/about" element={<About />}/>
-    <Route path="*" element={<NotFound />} />
-    </Routes>
-    </BrowserRouter> */}
+      {/* <NavBar /> */}
+      {/* <HomePage /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie/:id" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
-// import './HomePage.scss'
+import './Homepage.scss'
 import NavBar from "../../components/NavBar/NavBar";
 import MovieCard from "../../components/MovieCard";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
-// THIS ONE DOESNT WORK
 
 function HomePage() {
     return (
         <>
         <NavBar />
-        <SearchBar />
-        <MovieCard />
+        <div className="homepage">
+        <form className="homepage__form">
+          <input className="homepage__input"type="text" placeholder="Search here..."></input>
+        </form>
+  
+
+      <div className='moviecard'>
+      {/* <img className="moviecard__img" src={avatar} alt="movie poster">Picture</img> */}
+        <div className="moviecard__title">title</div>
+        <div className="moviecard__details">Summary details button to movie details page</div>
+       </div>
+       </div>
         </>
     )
 }
